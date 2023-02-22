60b03688d473958d77902ef536a878afd97d4836
unknown <7221798@gmail.com>
2023-02-22 22:57:59 +0300

    3253_Merge Sorted Array_test.go

---
+++ leetcode/3253_Merge Sorted Array_test.go
@@ -0,0 +1,136 @@
+package main_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+
+   Hide Hint #1
+You can easily solve this problem if you simply think about two elements at a time rather than two arrays. We know that each of the individual arrays is sorted. What we don't know is how they will intertwine. Can we take a local decision and arrive at an optimal solution?
+   Hide Hint #2
+If you simply consider one element each at a time from the two arrays and make a decision and proceed accordingly, you will arrive at the optimal solution.
+*/
+
+func shiftArray(arr []int, fromPos int) {
+	if fromPos >= len(arr)-1 {
+		return
+	}
+
+	for i := len(arr) - 2; i >= fromPos; i-- {
+		arr[i+1] = arr[i]
+	}
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := 0
+	j := 0
+
+	for i <= len(nums1)-1 && j <= n-1 {
+
+		if nums1[i] <= nums2[j] && i < m+j {
+			i++
+		} else {
+			shiftArray(nums1, i)
+			nums1[i] = nums2[j]
+			i++
+			j++
+		}
+	}
+}
+
+func Test_3253_shiftArray1(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	shiftArray(arr, 4)
+	arr[4] = -1
+
+	if !intArraysIsEqual(arr, []int{1, 2, 3, 4, -1, 5}) {
+		t.Fatal()
+	}
+
+}
+
+func Test_3253_Example1(t *testing.T) {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+
+	merge(nums1, 3, nums2, len(nums2))
+	fmt.Println(fmt.Sprintf("%v", nums1))
+
+	if !intArraysIsEqual(nums1, []int{1, 2, 2, 3, 5, 6}) {
+		t.Fatal(fmt.Sprintf("%v", nums1))
+	}
+}
+
+func Test_3253_Example2(t *testing.T) {
+	nums1 := []int{1}
+	nums2 := []int{}
+
+	merge(nums1, 1, nums2, len(nums2))
+
+	if !intArraysIsEqual(nums1, []int{1}) {
+		t.Fatal(fmt.Sprintf("%v", nums1))
+	}
+}
+
+func Test_3253_Example3(t *testing.T) {
+	nums1 := []int{0}
+	nums2 := []int{1}
+
+	merge(nums1, 0, nums2, len(nums2))
+
+	if !intArraysIsEqual(nums1, []int{1}) {
+		t.Fatal(fmt.Sprintf("%v", nums1))
+	}
+}
+
+func Test_3253_Example4(t *testing.T) {
+	nums1 := []int{-1, 0, 0, 3, 3, 3, 0, 0, 0}
+	nums2 := []int{1, 2, 2}
+
+	merge(nums1, 6, nums2, len(nums2))
+
+	if !intArraysIsEqual(nums1, []int{-1, 0, 0, 1, 2, 2, 3, 3, 3}) {
+		t.Fatal(fmt.Sprintf("%v", nums1))
+	}
+}
