22af6195249dd300242e8a3856a9594e66ca775c
unknown <7221798@gmail.com>
2023-02-22 22:20:53 +0300

    common additional tests

--- leetcode/common_test.go
+++ leetcode/common_test.go
@@ -1,5 +1,7 @@
 package main_test
 
+import "testing"
+
 func intArraysIsEqual(first, second []int) bool {
 	if len(first) != len(second) {
 		return false
@@ -10,4 +12,34 @@ func intArraysIsEqual(first, second []int) bool {
 		}
 	}
 	return true
+}
+
+func Test_intArraysIsEqual_Example1(t *testing.T) {
+	if !intArraysIsEqual([]int{}, []int{}) {
+		t.Fatal()
+	}
+}
+
+func Test_intArraysIsEqual_Example2(t *testing.T) {
+	if !intArraysIsEqual([]int{1}, []int{1}) {
+		t.Fatal()
+	}
+}
+
+func Test_intArraysIsEqual_Example3(t *testing.T) {
+	if intArraysIsEqual([]int{1}, []int{2}) {
+		t.Fatal()
+	}
+}
+
+func Test_intArraysIsEqual_Example4(t *testing.T) {
+	if !intArraysIsEqual([]int{1, 2, 3}, []int{1, 2, 3}) {
+		t.Fatal()
+	}
+}
+
+func Test_intArraysIsEqual_Example5(t *testing.T) {
+	if intArraysIsEqual([]int{1, 2, 5}, []int{1, 2, 3}) {
+		t.Fatal()
+	}
 }
