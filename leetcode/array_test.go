7ed21e646f434c137d1a0d3952c8b416b298833a
unknown <7221798@gmail.com>
2023-02-26 17:58:59 +0300

    functions for sorted array for fast search and insert

--- leetcode/array_test.go
+++ leetcode/array_test.go
@@ -40,6 +40,72 @@ func IsEqualStringArrays(first, second []string) bool {
 	return true
 }
 
+func SearchInsertPosInSortedIntArrayWithRight(nums *[]int, target int, right int) int {
+	if len(*nums) == 0 {
+		return 0
+	}
+	if target <= (*nums)[0] {
+		return 0
+	}
+	if target > (*nums)[len(*nums)-1] {
+		return len(*nums)
+	}
+	left := 0
+	for {
+		p := (left + right) / 2
+		if target > (*nums)[p] {
+			left = p
+		}
+		if target <= (*nums)[p] {
+			right = p
+		}
+		if left == right-1 {
+			return right
+		}
+	}
+}
+
+func SearchInsertPosInSortedIntArray(nums *[]int, target int) int {
+	return SearchInsertPosInSortedIntArrayWithRight(nums, target, len(*nums)-1)
+}
+
+func InstertIntoSortedIntArray(array *[]int, target int) {
+	pos := SearchInsertPosInSortedIntArray(array, target)
+	if pos == len(*array) {
+		*array = append(*array, target)
+		return
+	}
+
+	*array = append((*array)[:pos+1], (*array)[pos:]...)
+	(*array)[pos] = target
+}
+
+func RemoveFromSortedIntArray(array *[]int, target int) {
+	pos := SearchInsertPosInSortedIntArray(array, target)
+	if pos < len(*array) {
+		*array = append((*array)[:pos], (*array)[pos+1:]...)
+	}
+}
+
+func RemoveDuplicatesInSortedIntArray(nums *[]int) {
+
+	if len(*nums) < 2 {
+		return
+	}
+
+	ic := 0 // current sorted index
+
+	for i := 1; i < len(*nums); i++ {
+
+		if (*nums)[i] > (*nums)[ic] {
+			ic++
+			(*nums)[ic] = (*nums)[i]
+		}
+	}
+
+	(*nums) = (*nums)[:ic+1]
+}
+
 func TwoDimensionalByteArray2String(array [][]byte) string {
 	result := ""
 
@@ -51,6 +117,32 @@ func TwoDimensionalByteArray2String(array [][]byte) string {
 	}
 
 	return result
+}
+
+func GetIntArrayMaximum(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	max := nums[0]
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func GetIntArrayMinimum(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	min := nums[0]
+	for _, num := range nums {
+		if num < min {
+			min = num
+		}
+	}
+	return min
 }
 
 func Test_IsEqualIntArrays_Example1(t *testing.T) {
