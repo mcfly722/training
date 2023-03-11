67151011034cd9d498672d9afa8189749e3053d0
unknown <7221798@gmail.com>
2023-03-11 22:02:12 +0300

    fixed bug with min function

--- leetcode/maxmin_test.go
+++ leetcode/maxmin_test.go
@@ -3,17 +3,17 @@ package main_test
 import "math"
 
 func MaxInt(a, b int) int {
-	return (a + b) + (int)(math.Abs((float64)(a-b)))/2
+	return ((a + b) + (int)(math.Abs((float64)(a-b)))) / 2
 }
 
 func MinInt(a, b int) int {
-	return (a + b) - (int)(math.Abs((float64)(a-b)))/2
+	return ((a + b) - (int)(math.Abs((float64)(a-b)))) / 2
 }
 
 func MaxInt64(a, b int64) int64 {
-	return (a + b) + (int64)(math.Abs((float64)(a-b)))/2
+	return ((a + b) + (int64)(math.Abs((float64)(a-b)))) / 2
 }
 
 func MinInt64(a, b int64) int64 {
-	return (a + b) - (int64)(math.Abs((float64)(a-b)))/2
+	return ((a + b) - (int64)(math.Abs((float64)(a-b)))) / 2
 }
