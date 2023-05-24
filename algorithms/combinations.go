e7f54f43e39333d5fc7dfcf2d2c8c8d4c5815e8a
mcfly722 <7221798@gmail.com>
2023-05-24 23:30:38 +0300

    NextSequence function with benchmark

--- algorithms/combinations.go
+++ algorithms/combinations.go
@@ -6,3 +6,19 @@ import "C"
 func Multiply(a int, b int) int {
 	return int(C.Multiply(C.int(a), C.int(b)))
 }
+
+func NextSequence(array *[]byte, digits byte) bool {
+	if len(*array) == 0 {
+		return false
+	}
+
+	if (*array)[0] < digits {
+		(*array)[0]++
+		return true
+	}
+
+	(*array)[0] = 0
+
+	newArr := (*array)[1:]
+	return NextSequence(&newArr, digits)
+}
--- algorithms/combinations_test.go
+++ algorithms/combinations_test.go
@@ -6,7 +6,40 @@ import (
 	"testing"
 )
 
-func Test_algo_0001(t *testing.T) {
-	r := algorithms.Multiply(5, 6)
-	t.Logf(fmt.Sprintf("%v", r))
+func Test_Multiply(t *testing.T) {
+	a := 5
+	b := 6
+	r := algorithms.Multiply(a, b)
+	if r != a*b {
+		t.Fatalf(fmt.Sprintf("obtained wrong answer %v*%v=%v", a, b, r))
+	}
+}
+
+func Test_NextSequence(t *testing.T) {
+	array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	i := 0
+	for {
+		//t.Logf(fmt.Sprintf("%5v %v", i, array))
+		if !algorithms.NextSequence(&array, 3) {
+			break
+		}
+		i++
+	}
+	t.Logf(fmt.Sprintf("total operations: %7v", i))
+
+}
+
+func Benchmark_NextSequence(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	operation:
+		for {
+			if !algorithms.NextSequence(&array, 3) {
+				break operation
+			}
+		}
+	}
 }
