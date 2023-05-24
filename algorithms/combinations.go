891f761faf0d34240f8f4fb33b59c8835654c6b7
mcfly722 <7221798@gmail.com>
2023-05-25 00:38:14 +0300

    fastest realization for sequences

--- algorithms/combinations.go
+++ algorithms/combinations.go
@@ -7,12 +7,27 @@ func Multiply(a int, b int) int {
 	return int(C.Multiply(C.int(a), C.int(b)))
 }
 
-func NextSequence(array *[]byte, digits byte) bool {
+func NextSequenceFastest(array *[]byte, digits byte) bool {
+
+	for i := 0; (*array)[i] < digits; i++ {
+		(*array)[i]++
+
+		if (*array)[i] < digits {
+			return true
+		}
+		(*array)[i] = 0
+	}
+
+	return false
+}
+
+func NextSequenceRecursive(array *[]byte, digits byte) bool {
+
 	if len(*array) == 0 {
 		return false
 	}
 
-	if (*array)[0] < digits {
+	if (*array)[0] < digits-1 {
 		(*array)[0]++
 		return true
 	}
@@ -20,5 +35,5 @@ func NextSequence(array *[]byte, digits byte) bool {
 	(*array)[0] = 0
 
 	newArr := (*array)[1:]
-	return NextSequence(&newArr, digits)
+	return NextSequenceRecursive(&newArr, digits)
 }
--- algorithms/combinations_test.go
+++ algorithms/combinations_test.go
@@ -15,13 +15,22 @@ func Test_Multiply(t *testing.T) {
 	}
 }
 
-func Test_NextSequence(t *testing.T) {
-	array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+func Test_NextSequenceRecursive_ShowSeveral(t *testing.T) {
+	array := []byte{0, 0, 0, 0, 5}
+	for i := 0; ; i++ {
+		t.Logf(fmt.Sprintf("%7v %v", i, array))
+		if !algorithms.NextSequenceRecursive(&array, 3) {
+			break
+		}
+	}
+}
+
+func Test_NextSequenceRecursive_Count(t *testing.T) {
+	array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}
 
 	i := 0
 	for {
-		//t.Logf(fmt.Sprintf("%5v %v", i, array))
-		if !algorithms.NextSequence(&array, 3) {
+		if !algorithms.NextSequenceRecursive(&array, 4) {
 			break
 		}
 		i++
@@ -30,14 +39,52 @@ func Test_NextSequence(t *testing.T) {
 
 }
 
-func Benchmark_NextSequence(b *testing.B) {
+func Benchmark_NextSequenceRecursive(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
-		array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}
 
 	operation:
 		for {
-			if !algorithms.NextSequence(&array, 3) {
+			if !algorithms.NextSequenceRecursive(&array, 4) {
+				break operation
+			}
+		}
+	}
+}
+
+func Test_NextSequenceFastest_ShowSeveral(t *testing.T) {
+	array := []byte{0, 0, 0, 0, 5}
+	for i := 0; ; i++ {
+		t.Logf(fmt.Sprintf("%7v %v", i, array))
+		if !algorithms.NextSequenceFastest(&array, 3) {
+			break
+		}
+	}
+}
+
+func Test_NextSequenceFastest_Count(t *testing.T) {
+	array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}
+
+	i := 0
+	for {
+		if !algorithms.NextSequenceFastest(&array, 4) {
+			break
+		}
+		i++
+	}
+	t.Logf(fmt.Sprintf("total operations: %7v", i))
+
+}
+
+func Benchmark_NextSequenceFastest(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		array := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}
+
+	operation:
+		for {
+			if !algorithms.NextSequenceFastest(&array, 4) {
 				break operation
 			}
 		}
